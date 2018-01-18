586f6969f1f408ccf6e2409dd26c3716c2376d33
THOURY BRYAN <bryan.thoury@live.fr>
2018-01-18 21:16:39 +0100

    AQUARIUM V0.0.2 TEST
    tentative de resolution des probléme

--- AQUARIUM/AQUARIUM/AQUARIUM.ino
+++ AQUARIUM/AQUARIUM/AQUARIUM.ino
@@ -1,22 +1,28 @@
-// AQUARIUM V0.0.1
+// AQUARIUM V0.0.2 TEST
 // BY Jérémy Cheynet et Bryan Thoury
 //
 
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include "RTClib.h"
+#include <DallasTemperature.h> //Librairie du capteur
 
-// Create object RTC
+           //DS18B20
+OneWire oneWire(3); //Bus One Wire sur la pin 3 de l'arduino
+DallasTemperature sensors(&oneWire); //Utilistion du bus Onewire pour les capteurs
+DeviceAddress sensorDeviceAddress; //Vérifie la compatibilité des capteurs avec la librairie
+
+// Créz l'objet RTC
 RTC_DS1307 RTC;
 
-// Create object lcd (I2C)
+// Crée l'objet lcd (I2C)
 LiquidCrystal_I2C lcd(0x27,16,2);
 
 void setup() {
-  // Init serial and print message
+  // Init le message série et imprimer
   Serial.begin(9600);
   Serial.println("Start program");
-  Serial.println("V0.0.1");
+  Serial.println("V0.0.2 TEST");
 
   // Init I2C
   Wire.begin();
@@ -27,7 +33,7 @@ void setup() {
     Serial.println("RTC n est pas en cours de fonctionnement!");
   }
 
-  // Init lcd and print message
+  // Init lcd et imprime le message
   lcd.init();
   lcd.backlight();
   lcd.setCursor(1,0);
@@ -37,6 +43,14 @@ void setup() {
   delay(5000);
   lcd.setCursor(1,0);
   lcd.print("nous sommes le");
+
+            //DS18B20
+
+Serial.begin(9600); //Permet la communication en serial
+ sensors.begin(); //Activation des capteurs
+ sensors.getAddress(sensorDeviceAddress, 0); //Demande l'adresse du capteur à l'index 0 du bus
+ sensors.setResolution(sensorDeviceAddress, 12); //Résolutions possibles: 9,10,11,12
+
 }
 
 void loop() {
@@ -48,13 +62,21 @@ void loop() {
   snprintf(buffer, 199, "%02d/%02d/%02d %02d:%02d   ", now.year()-2000,
       now.month(), now.day(), now.hour(), now.minute(), now.second());
 
-  // Display DateTime
+  // Afficher DateTime
   lcd.setCursor(0, 1);  
   lcd.print(buffer);
 
-  // Print DateTime on serial
+  // Imprimer DateTime en série
   Serial.println(buffer);
 
-  // Wait 100ms
+  // attend 100ms
   delay(100);
+
+            //DS18B20
+
+void loop()  {
+ sensors.requestTemperatures(); //Demande la température aux capteurs
+ Serial.print("La température est: ");
+ Serial.print(sensors.getTempCByIndex(0)); //Récupération de la température en celsius du capteur n°0
+ Serial.println(" C°");
 }
